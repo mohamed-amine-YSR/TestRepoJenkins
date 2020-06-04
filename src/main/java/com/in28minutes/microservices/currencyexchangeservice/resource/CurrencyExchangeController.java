9ba645901636c6d2f15ace9efc163afc687121a4
mohamed-amine-YSR <47361342+mohamed-amine-YSR@users.noreply.github.com>
2020-06-05 00:00:24 +0100

    Update CurrencyExchangeController.java

--- src/main/java/com/in28minutes/microservices/currencyexchangeservice/resource/CurrencyExchangeController.java
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/resource/CurrencyExchangeController.java
@@ -25,7 +25,7 @@ public class CurrencyExchangeController {
 
 	@GetMapping("/")
 	public String imHealthy() {
-		return "{healthyaaa:true}";
+		return "{healthy:true}";
 	}
 
 	//http://localhost:8000/currency-exchange/from/USD/to/INR
